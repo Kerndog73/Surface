5cc962817ec1f23afffb031b5eb8d12e9f08de27
Kerndog73 <kerndog73@gmail.com>
2017-12-27 14:58:16 +1030

    Setup project

---
+++ Surface/surface.cpp
@@ -0,0 +1,14 @@
+//
+//  surface.cpp
+//  Surface
+//
+//  Created by Indi Kernick on 27/12/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include "surface.hpp"
+
+//Coolest symbol in town
+void coolSymbol() {
+  
+}
---
+++ Surface/surface.hpp
@@ -0,0 +1,14 @@
+//
+//  surface.hpp
+//  Surface
+//
+//  Created by Indi Kernick on 27/12/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef surface_hpp
+#define surface_hpp
+
+
+
+#endif
